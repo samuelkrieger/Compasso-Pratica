e22de7ae6f1a19f31b13892b5d7eb23708596dae
Jeferson Menchik <juniomenchik@live.com>
2021-05-19 14:31:11 -0300

    Init Aluno

---
+++ src/main/java/com/compasso/domain/Aluno.java
@@ -0,0 +1,14 @@
+package com.compasso.domain;
+
+
+import javax.persistence.Entity;
+
+import javax.persistence.Table;
+
+@Entity
+@Table(name= "aluno")
+public class Aluno {
+
+    private String Nome;
+
+}
