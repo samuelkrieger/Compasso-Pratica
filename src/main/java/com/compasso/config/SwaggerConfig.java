11ae0aa91fe59ff9dfcef0e65c5a7b9a67f1f381
Samuel Krieger Bonini <samuel.kriegerb@gmail.com>
2021-05-21 14:34:41 -0300

    swagger corrigido

--- src/main/java/com/compasso/config/SwaggerConfig.java
+++ src/main/java/com/compasso/config/SwaggerConfig.java
@@ -16,7 +16,7 @@ public class SwaggerConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("br.com.compasso"))
+                .apis(RequestHandlerSelectors.basePackage("com.compasso"))
                 .paths(PathSelectors.any())
                 .build();
     }
